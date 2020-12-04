f895af13a355518a0d695e39c322528e9c3bfb83
IsaacDSC <isaac8.silva@hotmail.com>
2020-12-04 18:26:44 -0300

    arrumando acesss control

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ wsServer.on('connection', (ws, req) => {
 
 app.use((req, res, next) => {
     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "10.0.0.164");
+    res.header("Access-Control-Allow-Origin", "*");
     //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
